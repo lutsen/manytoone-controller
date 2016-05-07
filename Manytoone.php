53913000ef692b758bbf9b10316ea44dc44adbc3
Lútsen Stellingwerff <lutsen@hoverkraft.nl>
2016-05-07 17:41:00 +0100

    First!

---
+++ Manytoone.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Lagan\Property;
+
+/**
+ * Controller for the Lagan mamy-to-one property.
+ * Lets the user define a may-to-one relation between two content objects.
+ * The Manytoone property type controller enables a many-to-one relation between 2 Lagan models,
+ * as described here: http://redbeanphp.com/index.php?p=/many_to_one
+ * The name of the property should be the name of the Lagan model this model can have a many-to-one
+ * relation with. For this to work properly the other model should have a one-to-many relation with this model.
+ * So in our example in the Lagan project the LaganCrew model has a many-to-one relation
+ * with the LaganHoverkraft model, and the LaganHoverkraft model has a one-to-many relation
+ * with the LaganCrew model.
+ *
+ * A property type controller can contain a set, read, delete and options method. All methods are optional.
+ * To be used with Lagan: https://github.com/lutsen/lagan
+ */
+
+class Manytoone {
+
+	/**
+	 * The set method is executed each time a property with this type is set.
+	 *
+	 * @param bean		$bean		The Redbean bean object with the property.
+	 * @param array		$property	Lagan model property arrray.
+	 * @param integer	$new_value	The id of the object the object with this property has a many-to-one relation with.
+	 *
+	 * @return bean		The Redbean bean object the object with this property has a many-to-one relation with.
+	 */
+	public function set($bean, $property, $new_value) {
+
+		// Check and set relation
+		$relation = \R::findOne( $property['name'], ' id = :id ', [ ':id' => $new_value ] );
+		if ( !$relation ) {
+			throw new Exception('This '.$property['name'].' does not exist.');
+		} else {
+			return $relation;
+		}
+
+	}
+
+	/**
+	 * The options method returns all the optional values this property can have.
+	 *
+	 * @param array		$property	Lagan model property arrray.
+	 *
+	 * @return array	Array with all beans of the $property['name'] Lagan model.
+	 */
+	public function options($property) {
+		return \R::findAll( $property['name'] );
+	}
+
+}
+
+?>
---
+++ README.md
@@ -0,0 +1,15 @@
+[<img src="https://cdn.rawgit.com/lutsen/lagan/master/lagan-logo.svg" width="100" alt="Lagan">](https://github.com/lutsen/lagan)
+
+Lagan Many-to-one Property
+==========================
+
+ Controller for the Lagan mamy-to-one property.  
+ Lets the user define a may-to-one relation between two content objects.
+ 
+ The Manytoone property type controller enables a many-to-one relation between 2 Lagan models, as described here: http://redbeanphp.com/index.php?p=/many_to_one. The name of the property should be the name of the Lagan model this model can have a many-to-one relation with. For this to work properly the other model should have a one-to-many relation with this model. So in our example in the Lagan project the LaganCrew model has a many-to-one relation with the LaganHoverkraft model, and the LaganHoverkraft model has a one-to-many relation with the LaganCrew model.
+
+A property type controller can contain a set, read, delete and options method. All methods are optional.
+
+To be used with [Lagan](https://github.com/lutsen/lagan). Lagan lets you create flexible content objects with a simple class, and manage them with a web interface.
+
+Lagan is a project of [Lútsen Stellingwerff](http://lutsen.land/) from [HoverKraft](http://www.hoverkraft.nl/).
